0860842ecdc5c1262ec6ad53b42d78a722e6da03
gargoyle777 <41432392+gargoyle777@users.noreply.github.com>
2023-01-12 09:34:24 +0100

    recovered file

---
+++ workerThread.h
@@ -0,0 +1,23 @@
+#ifndef WORKERTHREAD_H_
+#define WORKERTHREAD_H_
+
+struct queueEl
+{
+    char *filename;
+    struct queueEl *next;
+};
+typedef struct queueEl node;
+
+struct arguments
+{
+    struct queueEl *queueHead;
+    int *queueSize;
+    pthread_mutex_t *mtx;
+    pthread_cond_t *queueNotFull;
+    pthread_cond_t *queueNotempty;
+    int *exitReq;
+};
+
+void* worker(void *arg);
+
+#endif
